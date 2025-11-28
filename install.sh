dbf3011a433cef2db3090ecd5d1dffaddc066866
Glenn Fiedler <glenn@mas-bandwidth.com>
2025-11-28 10:58:22 -0500

    install xdp-tools on install

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo journalctl --vacuum-size 10M
 sudo DEBIAN_FRONTEND=noninteractive NEEDRESTART_SUSPEND=1 apt autoremove -y
 sudo DEBIAN_FRONTEND=noninteractive NEEDRESTART_SUSPEND=1 apt update -y
 sudo DEBIAN_FRONTEND=noninteractive NEEDRESTART_SUSPEND=1 apt full-upgrade -y
-sudo DEBIAN_FRONTEND=noninteractive NEEDRESTART_SUSPEND=1 apt install libcurl3-gnutls-dev build-essential vim wget libsodium-dev flex bison clang unzip libc6-dev-i386 gcc-12 dwarves libelf-dev pkg-config m4 libpcap-dev net-tools libbpf-dev libxdp-dev -y
+sudo DEBIAN_FRONTEND=noninteractive NEEDRESTART_SUSPEND=1 apt install libcurl3-gnutls-dev build-essential vim wget libsodium-dev flex bison clang unzip libc6-dev-i386 gcc-12 dwarves libelf-dev pkg-config m4 libpcap-dev net-tools libbpf-dev libxdp-dev xdp-tools -y
 sudo DUBAIN_FRONTEND=noninteractive NEEDRESTART_SUSPEND=1 apt install dwarves linux-headers-`uname -r` linux-tools-`uname -r` -y
 sudo DEBIAN_FRONTEND=noninteractive NEEDRESTART_SUSPEND=1 apt autoremove -y
 
